5b3827ccf58c06dc6ed2686b90db9f025f3ecb11
Dylan Walker <djwalker200@gmail.com>
2023-03-12 10:42:32 -0400

    Added Dataloader
    Added MNIST dataloader

--- treeNN/main.py
+++ treeNN/main.py
@@ -2,6 +2,7 @@ from keras.datasets import mnist
 import numpy as np  
 import matplotlib.pyplot as plt
 import torch
+import torchvision
 from trainer import Trainer
 import yaml
 
@@ -11,8 +12,28 @@ with open('configs.yaml', 'r') as f:
 
 ## LOAD DATASET
 # (train_X, train_y), (test_X, test_y) = mnist.load_data()
-train_dataloader = DataLoader(train_X, train_y)
-test_dataloader = DataLoader(test_X, test_y)
+train_dataloader = torchvision.datasets.MNIST(
+    'datasets/MNIST',
+    train=True,
+    download=True,
+    )
+test_dataloader = torchvision.datasets.MNIST(
+    'datasets/MNIST',
+    train=False,
+    download=True,
+    )
+
+train_dataloader = torch.utils.data.DataLoader(train_dataset,
+                                          batch_size=configs['train']['batch_size'],
+                                          shuffle=True,
+                                          num_workers=configs['train']['num_workers'])
+
+test_dataloader =   torch.utils.data.DataLoader(train_dataset,
+                                          batch_size=configs['test']['batch_size'],
+                                          shuffle=True,
+                                          num_workers=configs['test']['num_workers'])                                    
+
+
 ## Create Trainer
 trainer = Trainer(configs)
 
@@ -22,8 +43,8 @@ trainer = Trainer(configs)
 
 ## EVALUATE
 
-train_losses = trainer.evaluate(train_dataloader)
-eval_losses = trainer.evaluate(test_dataloader)
+#train_losses = trainer.evaluate(train_dataloader)
+#seval_losses = trainer.evaluate(test_dataloader)
 
 
 
